0a656da8393468a22b470f2731df873e15365a49
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2025-01-12 00:39:52 +0900

    init

---
+++ ikm/README.md
@@ -0,0 +1,7 @@
+# 【IKM】 このサイトにアクセスできません
+
+- [ICTSC2024 本戦 問題解説: 【IKM】 このサイトにアクセスできません]()
+
+## トポロジ
+
+![topology](./docs/topology.drawio.svg)
---
+++ ikm/ansible/roles/client/files/firefox.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import logging
+
+url = "https://www.ikm.internal/"
+
+logging.basicConfig(filename='firefox.log', level=logging.INFO,
+                    format='%(asctime)s %(levelname)s:%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+options = webdriver.FirefoxOptions()
+options.add_argument("-headless")
+options.set_preference("network.dns.native_https_query", True)
+
+try:
+  driver = webdriver.Firefox(options=options)
+  driver.get(url)
+  logging.info(f"successfully accessed {url}")
+except Exception as e:
+  logging.error(f"failed to access {url}: {e}")
+finally:
+  driver.quit()
