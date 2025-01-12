ce9d41959f5a5767ea0b366214c67e7ff4ab05c8
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2025-01-13 02:46:09 +0900

    edit python

--- ikm/ansible/roles/client/files/firefox.py
+++ ikm/ansible/roles/client/files/firefox.py
@@ -3,19 +3,26 @@ import logging
 
 url = "https://www.ikm.internal/"
 
-logging.basicConfig(filename='firefox.log', level=logging.INFO,
-                    format='%(asctime)s %(levelname)s:%(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    filename="firefox.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s:%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 options = webdriver.FirefoxOptions()
 options.add_argument("-headless")
 options.set_preference("network.dns.native_https_query", True)
+service = webdriver.FirefoxService(executable_path="/snap/bin/geckodriver")
+
+driver = None
 
 try:
-  driver = webdriver.Firefox(options=options)
-  driver.get(url)
-  logging.info(f"successfully accessed {url}")
+    driver = webdriver.Firefox(options=options, service=service)
+    driver.get(url)
+    logging.info(f"successfully accessed {url}")
 except Exception as e:
-  logging.error(f"failed to access {url}: {e}")
+    logging.error(f"failed to access {url}: {e}")
 finally:
-  driver.quit()
+    if driver:
+        driver.quit()
